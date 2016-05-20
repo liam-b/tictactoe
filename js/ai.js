df827c284d3145f4db23e3cb22fd3a833accfb5a
Liam Brennan <liam@anywhich.com>
2016-05-20 10:08:25 +1000

    trying to get ai working

--- js/ai.js
+++ js/ai.js
@@ -1,3 +1,5 @@
+lastMove = 'BLUE'
+
 function aiMove () {
   if (testForWin() == false) {
     minimax();
@@ -34,18 +36,30 @@ function moveRandom () {
 }
 
 function minimax () {
-  lastMove = BLUE;
-  for (a = 0; a < 3; a++) {
-    for (b = 0; b < 3; b++) {
-      if (board[a][b].color == GREY) {
-        board[a][b].color = RED;
-        if (testForWin() == RED) {
-          board[a][b].grade += 1;
+  console.log('minimax');
+  for (var a = 0; a < 3; a++) {
+    for (var b = 0; b < 3; b++) {
+      if (lastMove == BLUE) {
+        if (board[a][b].color == GREY) {
+          board[a][b].color = RED;
+          if (testForWin() == RED) {
+            board[a][b].grade += 1;
+            lastMove = RED;
+            minimax();
+          }
+        board[a][b].color = GREY;
         }
-      board[a][b].color = GREY;
       }
       else {
-        board[a][b].grade = -10;
+        if (board[a][b].color == GREY) {
+          board[a][b].color = BLUE;
+          if (testForWin() == BLUE) {
+            board[a][b].grade += -1;
+            lastMove = BLUE;
+            minimax();
+          }
+        board[a][b].color = GREY;
+        }
       }
     }
   }
@@ -55,7 +69,7 @@ function playBestMove () {
   bestMove = [ 0, 0, -100 ];
   for (a = 0; a < 3; a++) {
     for (b = 0; b < 3; b++) {
-      if (board[a][b].grade > bestMove[2]) {
+      if (board[a][b].grade > bestMove[2] && board[a][b].color == GREY) {
         bestMove = [a, b, board[a][b].grade];
       }
     }
