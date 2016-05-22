a57dc1f6d7bac47a3bfa1e1b3458d5da295efd21
Liam Brennan <liam@anywhich.com>
2016-05-23 09:13:49 +1000

    working on new board with better ai

--- js/game.js
+++ js/game.js
@@ -2,12 +2,13 @@ var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
 
 GREY = '#909090';
+BACKGROUND = '#d9d9d9';
 BLUE = '#4faddb';
 RED = '#db4f4f';
 
 WIDTH = 100;
 HEIGHT = 100;
-OFFSET = 115;
+OFFSET = 105;
 
 CENTER_X = canvas.width / 2;
 CENTER_Y = canvas.height / 2;
@@ -26,7 +27,7 @@ for (x = 0; x < 3; x++) {
   squareX = squareX + OFFSET;
   for (y = 0; y < 3; y++) {
     squareY = squareY + OFFSET;
-    board[x][y] = { color : GREY, x : squareX, y : squareY, grade : 0 }
+    board[x][y] = { owner : '', x : squareX, y : squareY }
   }
   squareY = CENTER_Y - OFFSET * 2;
 }
@@ -37,13 +38,11 @@ var mouse = {
 }
 
 function draw () {
-  if (turn == 'ai' && testForWin() != GREY) {
+  if (testForWin() == false && turn == 'ai') {
     aiMove();
   }
+  winCheck();
   drawBoard();
-  if (playing) {
-    winCheck();
-  }
   requestAnimationFrame(draw);
 }
 
@@ -60,14 +59,15 @@ function mousePosition (event) {
 }
 
 function mouseClick (event) {
-  if (turn == 'player' && playing) {
+  if (turn == 'player') {
     for (x = 0; x < 3; x++) {
       for (y = 0; y < 3; y++) {  
         if (mouse.x > board[y][x].x - WIDTH / 2 && mouse.x < board[y][x].x + WIDTH / 2) {
           if (mouse.y > board[y][x].y - HEIGHT / 2 && mouse.y < board[y][x].y + HEIGHT / 2) {
-            if (board[y][x].color == GREY) {
-              board[y][x].color = BLUE;
+            if (board[y][x].owner == '') {
+              board[y][x].owner = 'player';
               turn = 'ai'
+              move++;
             }
           }
         }
@@ -77,41 +77,44 @@ function mouseClick (event) {
   if (playing == false) {
     reset();
   }
-  move++;
 }
 
-function winCheck() {
-  if (testForWin() == BLUE) {
-    print('Blue wins', canvas.width / 2 - 110, 120, BLUE, 'center')
-    playing = false;
+function winCheck () {
+  winner = testForWin();
+  if (winner == BLUE) {
+    print('Blue wins', canvas.width / 2 - 110, 120, BLUE, 'lighter', '50px')
   }
-  else if (testForWin() == RED) {
-    print('Red wins', canvas.width / 2 - 100, 120, RED, 'center')
-    playing = false;
+  else if (winner == RED) {
+    print('Red wins', canvas.width / 2 - 100, 120, RED, 'lighter', '50px')
   }
-  else if (testForWin() == GREY) {
-    print('Draw', canvas.width / 2 - 60, 120, GREY, 'center')
+  else if (winner == GREY) {
+    print('Draw', canvas.width / 2 - 60, 120, GREY, 'lighter', '50px')
+  }
+  if (winner != false) {
     playing = false;
+    move = 0;
   }
 }
 
-function print (text, x, y, color, align) {
-  context.font = 'lighter 50px Open Sans';
+function print (text, x, y, color, type, size) {
+  context.font = type + ' ' + size + ' Open Sans';
   context.fillStyle = color;
   context.fillText(text, x, y);
 }
 
 function drawBoard () {
+  context.fillStyle = GREY;
+  context.fillRect(CENTER_X - 155, CENTER_Y - 155, 310, 310);
   for (x = 0; x < 3; x++) {
     for (y = 0; y < 3; y++) {
-      context.beginPath();
-      context.fillStyle = board[x][y].color;
+      context.fillStyle = BACKGROUND;
       context.fillRect(board[x][y].x - WIDTH / 2, board[x][y].y - HEIGHT / 2, HEIGHT, WIDTH);
-      context.closePath();
+      if (board[x][y].owner == 'player') {
+        print('X', board[x][y].x - 25, board[x][y].y + 30, BLUE, 'bold', '80px');
+      }
+      else if (board[x][y].owner == 'ai') {
+        print('O', board[x][y].x - 31, board[x][y].y + 30, RED, 'bold', '80px');
+      }
     }
   }
-}
-
-function clear () {
-  context.clearRect(0, 0, canvas.width, canvas.height);
 }
